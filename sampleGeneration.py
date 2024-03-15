ff3e6246953843da07f934cb6f4e4ee2a59444de
N/A <null@null.com>
2024-03-15 02:44:46 -0400

    Converted circle to ellipse for rotational variations

--- sampleGeneration.py
+++ sampleGeneration.py
@@ -1,23 +1,28 @@
+import numpy
 from PIL import Image, ImageDraw
 
 class Sample:
-    def __init__(self, sampleType, backgroundSize=None):
-        self.makeBackground()
-        if sampleType == "Square":
-            self.makeSquare()
-        elif sampleType == "Circle":
-            self.makeCircle()
+    def __init__(self, sampleType, **kwargs):
+        self.makeBackground(**kwargs)
+        self.sampleType = sampleType
+        if self.sampleType == "Square":
+            self.makeSquare(**kwargs)
+        elif self.sampleType == "Ellipse":
+            self.makeEllipse(**kwargs)
 
     def showSample(self):
         self.background.show()
 
-    def makeBackground(self, size=(150,150), color=(255,255,255)):
-        self.background = Image.new("RGB", size=size, color=color)
+    def makeNumpyVersion(self):
+        self.sample = numpy.asarray(self.background)
+
+    def makeBackground(self, backgroundSize=(150,150), backgroundColor=(255,255,255)):
+        self.background = Image.new("RGB", size=backgroundSize, color=backgroundColor)
     
     def makeSquare(self, x0=50, y0=50, width=50, height=50, color=(0,0,0)):
         drawing = ImageDraw.Draw(self.background)
         drawing.rectangle([(x0, y0),(x0+width, y0+height)], fill=color)
     
-    def makeCircle(self, x0=50, y0=50, width=50, height=50, color=(0,0,0)):
+    def makeEllipse(self, x0=25, y0=50, width=100, height=50, color=(0,0,0)):
         drawing = ImageDraw.Draw(self.background)
         drawing.ellipse([(x0, y0),(x0+width, y0+height)], fill=color)
--- scratchpad.py
+++ scratchpad.py
@@ -2,10 +2,21 @@
 import sampleGeneration
 temp = sampleGeneration.Sample("Square")
 temp.showSample()
+temp.makeNumpyVersion()
+print(temp.sample.shape)
 
 # %%
 import sampleGeneration
-temp = sampleGeneration.Sample("Circle")
+temp = sampleGeneration.Sample("Ellipse")
 temp.showSample()
+temp.makeNumpyVersion()
+print(temp.sample.shape)
 
+# %%
+samples = list()
+for i in range(10):
+    samples.append(sampleGeneration.Sample("Circle"))
+    samples.append(sampleGeneration.Sample("Square"))
+# %%
+samples[1].showSample()
 # %%
